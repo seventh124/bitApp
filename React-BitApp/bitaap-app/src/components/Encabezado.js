4755e5ddd0d7ded20589856e1721e2c6d836a15d
Jwilliammedina <william27_mr@hotmail.com>
2021-10-27 22:51:08 -0500

    Registro de Ventas React

---
+++ React-BitApp/bitaap-app/src/components/Encabezado.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Encabezado() {
+    return (
+        <div className ="centro">
+            {/* ---Titulo encabezado--- */}
+            <br></br>
+            
+            <h3 className="tituloEncabezado">¡Bienvenido(a) (Nombre del Vendedor)!</h3>
+            {/* ---Fin de Titulo encabezado--- */}
+         </div>
+    )
+}
+
+export default Encabezado
